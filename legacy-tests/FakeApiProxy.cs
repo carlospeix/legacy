6b898489592c264beae6c8f4fee7aa8b24b8c1f7
Carlos Peix <carlos.peix@gmail.com>
2020-10-28 16:47:19 -0300

    Primer test del servicio pasando

---
+++ legacy-tests/FakeApiProxy.cs
@@ -0,0 +1,16 @@
+﻿using Legacy.UI;
+
+namespace Legacy.Tests
+{
+    internal class FakeApiProxy : ApiProxy
+    {
+        private readonly dynamic _jsonData;
+
+        public FakeApiProxy(dynamic jsonData) : base(null)
+        {
+            _jsonData = jsonData;
+        }
+
+        public override dynamic GetData(string location) => _jsonData;
+    }
+}
---
+++ legacy-tests/LegacyServiceTests.cs
@@ -0,0 +1,22 @@
+﻿using Legacy.UI;
+using Newtonsoft.Json;
+using NUnit.Framework;
+
+namespace Legacy.Tests
+{
+    [TestFixture]
+    public class LegacyServiceTests
+    {
+        [Test]
+        public void SePuedeNavegarConSudeste()
+        {
+            var jsonConSudeste = JsonConvert.DeserializeObject<dynamic>(@"{
+                'list': [ { 'wind': { 'deg': 170 } } ]
+            }");
+
+            var service = Service.WithProxy(new FakeApiProxy(jsonConSudeste));
+
+            Assert.That(service.Navegamos("irrelevante"), Is.True);
+        }
+    }
+}
